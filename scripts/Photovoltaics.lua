15ec1621e3a6865c3a899c5caaf158365614bc9b
destroyedlolo <l_faillie@yahoo.com>
2015-08-09 13:43:43 +0200

    Final version

--- scripts/Photovoltaics.lua
+++ scripts/Photovoltaics.lua
@@ -5,28 +5,19 @@
 --
 -- Marcel's directives :
 -- *DPD=Production photovoltaïque
--- Topic=TeleInfo/Consommation/values/PAPP
+-- Topic=TeleInfo/Production/values/PAPP
 -- Func=CheckProduction
 --
 
 local lastval
-local ans
 
 function CheckProduction( topic, val )
-t = val
-if ans then
-	val = ans - val
-end
-ans = t
-print(val)
-
 	if val ~= 0 then
 		lastval = os.time()
 		Marcel.ClearAlert("Photovoltaïque")
 	else
 		if lastval then
-print('diff', os.difftime( os.time(), lastval ))
-			if os.difftime( os.time(), lastval ) > --[[ 24 * 60 * 60 ]] 5 then
+			if os.difftime( os.time(), lastval ) > 24 * 60 * 60 then
 				Marcel.RiseAlert("Photovoltaïque", "Pas de production depuis 24h\nVérifiez l'onduleur et le disjoncteur.")
 			end
 		end
