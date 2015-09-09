f075b6abd0d1cd01dba269f3a58b9219fc9f3277
destroyedlolo <l_faillie@yahoo.com>
2015-09-09 23:09:58 +0200

    Add Marcel's version checking

--- scripts/AllVerif.lua
+++ scripts/AllVerif.lua
@@ -1,3 +1,8 @@
 require "scripts/Photovoltaics"
 require "scripts/UPSBatCharge"
 require "scripts/PublishLoad"
+
+if tonumber(Marcel.Version()) < 3.3 then
+	print("*F* Marcel 3.3 or newer needed by Lua functions")
+	os.exit( 20 )
+end
