65ec7543438c2f9185d184d3bd2f2b5c70d7aeb8
destroyedlolo <l_faillie@yahoo.com>
2015-10-08 00:56:47 +0200

    Cleaning

--- src/Meteo.c
+++ src/Meteo.c
@@ -12,55 +12,6 @@
 
 #include "Meteo.h"
 
-	/* structure to decode json information */
-
-struct jsonstatus {
-	unsigned short int blocklevel;
-	unsigned short int tablelevel;
-	char instring;
-};
-
-char *readlabel(char **p){
-	if(**p == '"'){
-		char *lbl = ++(*p);
-		while( **p != '"' && **p)
-			++(*p);
-		if(**p){
-			**p = 0;
-			(*p)++;
-			return lbl;
-		} else	/* End of data */
-			return NULL;
-	} else {
-		if(verbose)
-			fprintf(stderr, "*E* readlabel(), expecting '\"' but got '%s'\n", *p);
-		return NULL;
-	}
-}
-
-int isddigit( char c ){
-	return( isdigit(c) || c == '.' );
-}
-
-char *skipdata(char *p){
-	if(!*p)
-		return NULL;
-	else if(*p == '"'){	/* reading a string */
-		while(*++p != '"'){
-			if(!*p) return NULL;
-		}
-		return ++p;
-	} else if(isddigit(*p)){	/* Reading a number */
-		while(isddigit(*++p)){
-			if(!*p) return NULL;
-		}
-		return p;
-	} else {
-		fprintf(stderr, "*E* skipdata() : Unknown character '%s'\n", p);
-		return NULL;
-	}
-}
-
 	/* Curl's
 	 * Storing downloaded information in memory
 	 * From http://curl.haxx.se/libcurl/c/getinmemory.html example
@@ -108,34 +59,6 @@ void *process_MeteoST(void *actx){
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
 		if((res = curl_easy_perform(curl)) == CURLE_OK){	/* Processing data */
-			char *p = chunk.memory;
-
-			while(*p != '{'){	/* search block beginning */
-				if(!*p)	/* end of data */
-					goto allread;
-				p++;
-			}
-			p++;
-
-			for(;;){
-				char *lbl = readlabel(&p);
-				if(!lbl)
-					goto allread;
-printf("lbl: '%s'\n", lbl);
-				if(*p != ':'){
-					fprintf(stderr, "*E* Label must be followed by a ':', got '%s'\n", p);
-					goto allread;
-				}
-				p++;	/* skip ':' */
-				if(!strcmp(lbl, "list"))	/* Label found */
-					break;
-				if(!(p = skipdata(p)))
-					goto allread;
-printf("skip : '%s'\n", p);
-			}
-puts(p);
-allread:
-			;
 		} else
 			fprintf(stderr, "*E* Querying meteo : %s\n", curl_easy_strerror(res));
 
