bf85bf2e6e8f5839a442f5cd801eeea1e414aa08
destroyedlolo <l_faillie@yahoo.com>
2016-01-22 22:27:51 +0100

    Clean Makefile

--- remake.sh
+++ remake.sh
@@ -2,4 +2,6 @@
 
 #this script is used to create a Makefile
 
-LFMakeMaker -v +g -Isrc/ -cc='gcc -Wall -O2 -DFREEBOX -DUPS -DMETEO -lcurl -lpthread -lpaho-mqtt3c -DLUA -llua `pkg-config --cflags json-c` `pkg-config --libs json-c` -std=c99' src/*.c -t=Marcel > Makefile
+cd src
+
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -O2 -DFREEBOX -DUPS -DMETEO -lcurl -lpthread -lpaho-mqtt3c -DLUA -llua `pkg-config --cflags json-c` `pkg-config --libs json-c` -std=c99' *.c -t=../Marcel > Makefile
