51b2c53389c0cdef1e00f41dc6f036a824491641
destroyedlolo <l_faillie@yahoo.com>
2016-04-29 22:56:25 +0200

    Initial work for RFXtrx implementation

--- remake.sh
+++ remake.sh
@@ -4,4 +4,4 @@
 
 cd src
 
-LFMakeMaker -v +f=Makefile -cc='gcc -Wall -O2 -DFREEBOX -DUPS -DMETEO -lcurl -lpthread -lpaho-mqtt3c -DLUA `pkg-config --cflags lua` `pkg-config --libs lua` `pkg-config --cflags json-c` `pkg-config --libs json-c` -std=c99' *.c -t=../Marcel > Makefile
+LFMakeMaker -v +f=Makefile -cc='gcc -Wall -O2 -DRFXTRX -DFREEBOX -DUPS -DMETEO -lcurl -lpthread -lpaho-mqtt3c -DLUA `pkg-config --cflags lua` `pkg-config --libs lua` `pkg-config --cflags json-c` `pkg-config --libs json-c` -std=c99' *.c -t=../Marcel > Makefile
