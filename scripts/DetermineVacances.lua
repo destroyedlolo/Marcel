4dfcf498bcb5836359345f03ea08cca9aef0f5e5
destroyedlolo <l_faillie@yahoo.com>
2016-05-17 15:27:24 +0200

    Add an example to determine if we are in holidays.

--- scripts/DetermineVacances.lua
+++ scripts/DetermineVacances.lua
@@ -3,7 +3,16 @@
 -- This function determines and then publish if we are holiday or not
 --
 
+require "tostring"
+
 function DetermineVacances( info )
-	print( json.decode(info) )
+	local res = json.decode(info)
+	print( universal_tostring(res) )
+
+	if res.weekend ~= "False" or res.holiday ~= "False" then
+		Marcel.MQTTPublish( "Profile/Mode", "Vacances", true)
+	else
+		Marcel.MQTTPublish( "Profile/Mode", "Travail", true )
+	end
 end
 
