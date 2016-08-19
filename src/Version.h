9117c0803ce532b33837d186300d37bcd4ce3e7c
destroyedlolo <l_faillie@yahoo.com>
2016-08-19 23:01:38 +0200

    Bump version to 6.00

--- src/Version.h
+++ src/Version.h
@@ -8,13 +8,14 @@
  * 10/06/2016	- LF - v5.01 - Handle 1w alarm
  * 24/07/2016	- LF - v5.02 - Handle offset for FFV
  * 13/08/2016	- LF - v5.03 - Add Out file
+ * 19/08/2016	- LF - v6.00 - handle disable
  */
 
 #ifndef MARCEL_VERSION_H
 #define MARCEL_VERSION_H
 
 
-#define VERSION "5.0301"	/* Need to stay numerique as exposed to Lua 
+#define VERSION "6.0000"	/* Need to stay numerique as exposed to Lua 
 							 * VV.SSMM :
 							 * 	VV - Version
 							 * 	SS - SubVersion
