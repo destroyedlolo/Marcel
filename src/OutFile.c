ae8767586f5335a3c7c7be530f010c8e684ced5d
destroyedlolo <l_faillie@yahoo.com>
2016-09-02 17:49:54 +0200

    Log replaced with publishLog()

--- src/OutFile.c
+++ src/OutFile.c
@@ -20,15 +20,15 @@
 
 void processOutFile( struct _OutFile *ctx, const char *msg){
 	if(ctx->disabled){
-		if(verbose)
-			printf("*I* Writing to OutFile '%s' is disabled\n", ctx->uid);
+		publishLog('I', "Writing to OutFile '%s' is disabled\n", ctx->uid);
 		return;
 	}
 
 #ifdef LUA
 	if(ctx->funcname && ctx->funcid == LUA_REFNIL){
 		if( (ctx->funcid = findUserFunc( ctx->funcname )) == LUA_REFNIL ){
-			fprintf(stderr, "*E* [%s] configuration error : user function \"%s\" is not defined\n*E*This thread is dying.\n", ctx->uid, ctx->funcname);
+			publishLog('E', "[%s] configuration error : user function \"%s\" is not defined", ctx->uid, ctx->funcname);
+			publishLog('F', "[%s] Dying", ctx->uid);
 			pthread_exit(NULL);
 		}
 	}
@@ -36,16 +36,16 @@ void processOutFile( struct _OutFile *ctx, const char *msg){
 
 	FILE *f=fopen( ctx->file, "w" );
 	if(!f){
-		fprintf(stderr, "*E* '%s' : %s\n", ctx->file, strerror(errno));
+		publishLog('E', "[%s] '%s' : %s", ctx->uid, ctx->file, strerror(errno));
 		return;
 	}
 	fputs(msg, f);
 	if(ferror(f)){
-		fprintf(stderr, "*E* '%s' : %s\n", ctx->file, strerror(errno));
+		publishLog('E', "[%s] '%s' : %s", ctx->uid, ctx->file, strerror(errno));
 		fclose(f);
 		return;
-	} else if(verbose)
-		printf("*I* [%s] '%s' written in '%s'\n", ctx->uid, msg, ctx->file);
+	} 
+	publishLog('I', "[%s] '%s' written in '%s'\n", ctx->uid, msg, ctx->file);
 	fclose(f);
 
 #ifdef LUA
