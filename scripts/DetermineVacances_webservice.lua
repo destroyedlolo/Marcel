4539fd10d0b3cccda2a1f1a28d8385e9cb5470f0
destroyedlolo <l_faillie@yahoo.com>
2018-08-19 02:14:37 +0200

    Don't use the webservice anymore for that

--- scripts/DetermineVacances_webservice.lua
+++ scripts/DetermineVacances_webservice.lua
@@ -1,6 +1,7 @@
 -- DetermineVacances
 -- 
 -- This function determines and then publish if we are holiday or not
+-- (not used anymore)
 --
 
 json = require("dkjson")
@@ -11,7 +12,28 @@ function _DetermineVacances( info, topic, offset )
 -- offset : offset in day to apply to current date in case
 -- 		we have to determine weekday (if info is NIL)
 
-	print("_DetermineVacances() BIDON")
+	local r = "Vacances"
+
+	if info then	-- Get from the webservice
+		local res = json.decode(info)
+
+		if res.weekend == "False" or res.holiday == "False" then
+			r = "Travail"
+		end
+	else
+		r = "Travail"
+	end
+
+		-- check if it wasn't bulshit
+	local t = os.time() + offset * 86400 -- 24 * 60 * 60
+	t = os.date('*t', t)['wday']
+	if t == 1 or t == 7 then
+		r = "Vacances"
+	else
+		r = "Travail"
+	end
+
+	Marcel.MQTTPublish( topic, r, true )
 end
 
 function DetermineVacancesAujourdHui( info )
