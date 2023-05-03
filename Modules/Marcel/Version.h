76e33ff9080d3e33ca69b6e9002976d9f61d0618
destroyedlolo <l_faillie@yahoo.com>
2023-05-03 22:03:55 +0200

    Bump sub version

--- Modules/Marcel/Version.h
+++ Modules/Marcel/Version.h
@@ -49,7 +49,7 @@
 #define MARCEL_VERSION_H
 
 
-#define MARCEL_VERSION "8.0015"	/* Need to stay numerique as exposed to Lua 
+#define MARCEL_VERSION "8.0016"	/* Need to stay numerique as exposed to Lua 
 							 * VV.SSMM :
 							 * 	VV - Version
 							 * 	SS - SubVersion
