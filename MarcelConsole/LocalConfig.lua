efa3b0fe5c18a5fa60a7df488e43a11620574a66
destroyedlolo <l_faillie@yahoo.com>
2024-07-06 21:59:05 +0200

    Starting ...

---
+++ MarcelConsole/LocalConfig.lua
@@ -0,0 +1,5 @@
+-- MarcelConsole configuration file
+-- all local configuration change have to be made here
+
+MQTT_URL='tcp://torchwood.chez.moi:1883' -- URL where to connect to the broker
+MARCEL = 'Marcel.tst'
