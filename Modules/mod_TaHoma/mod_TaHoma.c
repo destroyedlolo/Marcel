0277b5283d567097a255dfeff352a0bc9b6f8808
destroyedlolo <l_faillie@yahoo.com>
2026-03-11 19:52:31 +0100

    Add state_* directive

--- Modules/mod_TaHoma/mod_TaHoma.c
+++ Modules/mod_TaHoma/mod_TaHoma.c
@@ -159,9 +159,9 @@ static enum RC_readconf readconf(uint8_t mid, const char *l, struct Section **se
 			 * They MUST be refined here : mod_core's doesn't deal with
 			 * the same structure.
 			 */
-			 struct State_definition *state = (*(struct section_Device **)section)->States;
+			struct State_definition *state = (*(struct section_Device **)section)->States;
 
-			 if((arg = striKWcmp(l, "state_Topic="))){
+			if((arg = striKWcmp(l, "state_Topic="))){
 				acceptSectionDirective( *section, "state_Topic=" );
 
 				state->topic = strdup(arg);
@@ -170,6 +170,27 @@ static enum RC_readconf readconf(uint8_t mid, const char *l, struct Section **se
 				if(cfg.verbose)
 					publishLog('C', "\t\t\tTopic : '%s'", state->topic);
 
+				return ACCEPTED;
+			} else if((!strcmp(l,"state_Retained"))){
+				acceptSectionDirective(*section, "state_Retained");
+				state->retained = true;
+
+				if(cfg.verbose)	/* Be verbose if requested */
+					publishLog('C', "\t\t\tSent as retained");
+				return ACCEPTED;
+			} else if((!strcmp(l,"state_Disabled"))){
+				acceptSectionDirective(*section, "state_Disabled");
+				state->disabled = true;
+
+				if(cfg.verbose)	/* Be verbose if requested */
+					publishLog('C', "\t\t\tStarting DISABLED");
+				return ACCEPTED;
+			} else if((!strcmp(l,"state_DoNotSimulate"))){
+				acceptSectionDirective(*section, "state_DoNotSimulate");
+				state->dontSimulate = true;
+
+				if(cfg.verbose)	/* Be verbose if requested */
+					publishLog('C', "\t\t\tDISABLED if in simulation mode");
 				return ACCEPTED;
 			}
 
@@ -234,6 +255,12 @@ static bool acceptSDirective( uint8_t sec_id, const char *directive ){
 			 * mod_core first (and then rejected).
 			 */
 		if( !strcmp(directive, "state_Topic=") )
+			return true;	/* Accepted */
+		else if( !strcmp(directive, "state_Retained") )
+			return true;	/* Accepted */
+		else if( !strcmp(directive, "state_Disabled") )
+			return true;	/* Accepted */
+		else if( !strcmp(directive, "state_DoNotSimulate") )
 			return true;	/* Accepted */
 #if 0
 		else if( !strcmp(directive, "Func=") )
